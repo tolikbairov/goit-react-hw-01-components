e21577868cadb115f0d06536a3f65cc8172bcfe1
Anatolii Bairov <tolikbairov16@gmail.com>
2021-09-25 15:36:28 +0300

    react app

--- src/index.js
+++ src/index.js
@@ -1,17 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import ReactDOM from "react-dom";
+import App from "./App";
+import "./index.css";
+// (
+//   <Painting
+//     src={data.avatar}
+//     name={data.name}
+//     tag={data.tag}
+//     location={data.location}
+//     followers={data.stats.followers}
+//     views={data.stats.views}
+//     likes={data.stats.likes}
+//   />
+// ),
+ReactDOM.render(<App />, document.querySelector("#root"));
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById("root")
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
